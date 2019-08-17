bed0aa58f894be36582ddf16bde7abb2e398d5f3
kool2017 <luyuzjut@163.com>
2019-08-17 13:13:36 +0800

    1

--- src/components/page/dvc/record/RecordMng.vue
+++ src/components/page/dvc/record/RecordMng.vue
@@ -1,10 +1,141 @@
 <template>
-
+    <div class="recordMng">
+        <el-row :gutter="10">
+            <el-col :span="24">
+                <el-card class="condition-card">
+                    <span>
+                        <i class="el-icon-search"></i> 查询条件: </span>
+                    <hr class="split"/>
+                    <div class="card-context">
+                        <el-row :gutter="20">
+                            <el-col :span="2">
+                                记录类型:
+                            </el-col>
+                            <el-col :span="3">
+                                <el-select v-model="condition.recordType" size="small" clearable>
+                                    <el-option label="1-报修" value="1"></el-option>
+                                    <el-option label="2-派单" value="2"></el-option>
+                                    <el-option label="3-转派" value="3"></el-option>
+                                    <el-option label="4-接单" value="4"></el-option>
+                                    <el-option label="5-维修登记" value="5"></el-option>
+                                    <el-option label="6-停工" value="6"></el-option>
+                                    <el-option label="7-留言" value="7"></el-option>
+                                    <el-option label="8-完工" value="8"></el-option>
+                                    <el-option label="9-评价" value="9"></el-option>
+                                    <el-option label="10-关闭工单" value="10"></el-option>
+                                </el-select>
+                            </el-col>
+                            <el-col :span="2">
+                                <el-button type="primary" size="small" icon="el-icon-search" @click="query">查询
+                                </el-button>
+                            </el-col>
+                        </el-row>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+        <el-row :gutter="10">
+            <el-col :span="24">
+                <el-card class="list-card">
+                <span>
+                        <i class="el-icon-tickets"></i> 列表: </span>
+                    <hr class="split"/>
+                    <div class="card-context">
+                        <el-table :data="retList" border style="width: 100%" ref="retTable" highlight-current-row
+                                  @row-click="selectOne" height="400">
+                            <el-table-column prop="id" label="序号" width="80"></el-table-column>
+                            <el-table-column prop="recordType" label="记录类型" width="150"></el-table-column>
+                            <el-table-column prop="orderId" label="工单id" width="100"></el-table-column>
+                            <el-table-column prop="submitName" label="报修人" width="150"></el-table-column>
+                            <el-table-column prop="submitPhone" label="报修人手机号" width="80"></el-table-column>
+                            <el-table-column prop="repairUserName" label="维修人" width="150"></el-table-column>
+                            <el-table-column prop="repairPhone" label="维修人手机号" width="80"></el-table-column>
+                            <el-table-column prop="content" label="描述信息" width="150"></el-table-column>
+                            <el-table-column prop="createdTime" label="创建时间" width="160"></el-table-column>
+                            <el-table-column prop="updatedTime" label="更新时间" width="160"></el-table-column>
+                        </el-table>
+                        <div class="pagination">
+                            <el-pagination layout="total, sizes, prev, pager, next, jumper"
+                                           @size-change="handleSizeChange"
+                                           @current-change="handleCurrentChange" :page-size="page.pageSize"
+                                           :total="page.total" :page-count="page.pageCount"
+                                           :current-page="page.currentPage"
+                                           :page-sizes="[10, 50, 100]">
+                            </el-pagination>
+                        </div>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "RecordMng"
+        name: "RecordMng",
+        data() {
+            return {
+                condition: {},
+                retList: [],
+                page: {
+                    pageSize: 10,
+                    total: 0,
+                    pageCount: 0,
+                    currentPage: 1
+                }
+            }
+        },
+        created() {
+            this.query()
+        },
+        methods: {
+            query() {
+                this.page = {
+                    pageSize: 10,
+                    total: 0,
+                    pageCount: 0,
+                    currentPage: 1
+                }
+                this.queryPage();
+            },
+            queryPage() {
+                let self = this
+                var input = Object.assign({}, self.condition)
+                input.currentPage = self.page.currentPage
+                input.pageSize = self.page.pageSize
+                self.$http
+                    .post('/eops/device/get_record_page', input)
+                    .then((res) => {
+                        var pkgOut = res.data
+                        self.retList = pkgOut.data
+                        self.page.total = pkgOut.total
+                        self.page.pageCount = pkgOut.pageCount
+                        for (let index = 0; index < self.retList.length; index++) {
+                            let element = self.retList[index]
+                            element.createdTime = self.$moment(element.createdTime).format('YYYY-MM-DD HH:mm:ss')
+                            element.updatedTime = self.$moment(element.updatedTime).format('YYYY-MM-DD HH:mm:ss')
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                        self.$alert(err, '提示', {
+                            confirmButtonText: '确定',
+                            type: 'error'
+                        })
+                    })
+            },
+            handleSizeChange(pageSize) {
+                let self = this
+                self.page.pageSize = pageSize
+                self.page.currentPage = 1
+                self.queryPage()
+            },
+            handleCurrentChange(currentPage) {
+                let self = this
+                self.page.currentPage = currentPage
+                self.queryPage()
+            }
+        }
     }
 </script>
 
