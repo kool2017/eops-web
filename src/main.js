83260f5a2d922b1b113f6889a7ba593452ef7ba3
kool2017 <luyuzjut@163.com>
2019-10-29 18:41:47 +0800

    1

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import router from './router'
 import axios from 'axios'
 import uuid from 'uuid/v1'
 import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-default/index.css'    // 默认主题
-// import '../static/css/theme-green/index.css'       // 浅绿色主题
 import 'babel-polyfill'
 import './assets/icon/iconfont.css'
 import sha256 from 'sha256'
