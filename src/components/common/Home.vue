0517afe650e290416ee4ee5fa98e9baee221c78d
kool2017 <luyuzjut@163.com>
2019-12-19 17:23:18 +0800

    1

--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -23,13 +23,13 @@
 
         <!--</el-col>-->
         <!--</el-row>-->
-        <!--<div class="bottom">-->
-        <!--<div style="width:300px;margin:0 auto; padding:20px 0;">-->
-        <!--<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602009770" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="../../../static/img/beian.png" style="float:left;" />-->
-        <!--<p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">浙公网安备 33010602009770号</p>-->
-        <!--</a>-->
-        <!--</div>-->
-        <!--</div>-->
+        <div class="bottom">
+        <div style="width:300px;margin:0 auto; padding:20px 0;">
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602009770" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="../../../static/img/beian.png" style="float:left;" />
+        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">浙公网安备 33010602009770号</p>
+        </a>
+        </div>
+        </div>
     </div>
 </template>
 
@@ -171,14 +171,14 @@
         background-color: rgb(228, 237, 252);
     }
 
-    /*.bottom {*/
-    /*width: 100%;*/
-    /*margin: 0 auto;*/
-    /*padding: 0px;*/
-    /*position: absolute;*/
-    /*bottom: 0px;*/
-    /*!* left: 50%; *!*/
-    /*!* margin-left: -150px; *!*/
-    /*background-color: rgb(46, 46, 48);*/
-    /*}*/
+    .bottom {
+    width: 100%;
+    margin: 0 auto;
+    padding: 0px;
+    position: absolute;
+    bottom: 0px;
+    /* left: 50%; */
+    /* margin-left: -150px; */
+    background-color: rgb(46, 46, 48);
+    }
 </style>
--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -42,6 +42,13 @@
                 </el-tab-pane>
             </el-tabs>
         </div>
+        <div class="bottom">
+            <div style="width:300px;margin:0 auto; padding:20px 0;">
+                <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602009770" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="../../../static/img/beian.png" style="float:left;" />
+                    <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">浙公网安备 33010602009770号</p>
+                </a>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -188,6 +195,17 @@
     .login-btn button {
         width: 100%;
         height: 36px;
+    }
+
+    .bottom {
+        width: 100%;
+        margin: 0 auto;
+        padding: 0px;
+        position: absolute;
+        bottom: 0px;
+        /* left: 50%; */
+        /* margin-left: -150px; */
+        background-color: rgb(46, 46, 48);
     }
 
 </style>
